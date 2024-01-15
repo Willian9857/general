aefeba0fa39d48828a241bd57f4f3e28370e330c
Willian <william.silva@Vindi.com.br>
2024-01-15 16:01:24 -0300

    Ajusta rubocop

--- db/migrate/20240115172249_email.rb
+++ db/migrate/20240115172249_email.rb
@@ -1,8 +1,5 @@
-# rubocop:disable Style/Documentation
-
 class Email < ActiveRecord::Migration[7.0]
   def change
     add_column :clients, :email, :string, null: false, default: ''
   end
 end
-# rubocop:enable Style/Documentation
