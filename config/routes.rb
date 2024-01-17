6fd6d6f47b6ba265c7a586ee440d851c915cc549
Willian.Silva <106764502+Willian9857@users.noreply.github.com>
2024-01-17 15:51:50 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :clients do
     resources :deposits, only: [:new, :create]
   end
-  get 'dashboard', to: 'clients#dashboard'  
+
+  get 'dashboard', to: 'clients#dashboard'
 end
